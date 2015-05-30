828d06578c2f3aa4c5f600726759d87c4359705b
Jiayi Zheng <thebluber@gmail.com>
2015-05-30 22:45:25 +0200

    Use  in dashboard controller to check whether currentUser is set Update test in controller

--- src/app/controllers/dashboard.controller.js
+++ src/app/controllers/dashboard.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('MasterPlan')
-  .controller('DashboardCtrl', function($scope, User) {
-    $scope.gravatarHash = User.gravatarHash;
+  .controller('DashboardCtrl', function($scope, User, $cookieStore) {
+    if($cookieStore.get('current_user')) {
+      $scope.gravatarHash = $cookieStore.get('current_user').gravatarHash;
+    } else {
+      User.authenticate();
+    }
   });
--- src/test/controllers/dashboard.controller.spec.js
+++ src/test/controllers/dashboard.controller.spec.js
@@ -3,22 +3,35 @@
 describe('dashboard.controller', function() {
 
   var scope,
+      cookieStore,
       userService;
 
   beforeEach(module('MasterPlan'));
   beforeEach(inject(function($injector) {
     scope = $injector.get('$rootScope');
+    cookieStore = $injector.get('$cookieStore');
     userService = $injector.get('User');
-    spyOn(userService, 'gravatarHash').and.returnValue('12345');
+    spyOn(userService, 'authenticate');
   }));
 
-  it('should have gravatarHash as function in scope', inject(function($controller) {
+  it('should have gravatarHash in scope if currentUser is set in cookie', inject(function($controller) {
+    cookieStore.put('current_user', { gravatarHash: '12345' });
+
     $controller('DashboardCtrl', {
       $scope: scope,
+      $cookieStore: cookieStore,
       User: userService
     });
     expect(scope.gravatarHash).toBeDefined();
-    expect(scope.gravatarHash()).toEqual('12345');
-    expect(userService.gravatarHash).toHaveBeenCalled();
+    expect(scope.gravatarHash).toEqual('12345');
+  }));
+
+  it('should call authenticate if currentUser is not set in cookie', inject(function($controller) {
+    $controller('DashboardCtrl', {
+      $scope: scope,
+      $cookieStore: cookieStore,
+      User: userService
+    });
+    expect(userService.authenticate).toHaveBeenCalled();
   }));
 });
