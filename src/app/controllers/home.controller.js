e693f9f263a19dd1af4de7fac3a68e0fd8509481
Jiayi Zheng <thebluber@gmail.com>
2015-07-05 01:43:22 +0200

    Home controller is parent state of dashboard so there's no need of state transition to dashboard Clean up test

--- src/app/controllers/home.controller.js
+++ src/app/controllers/home.controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
 angular.module('MasterPlan')
-  .controller('HomeCtrl', function($state) {
-    $state.go('home.dashboard');
+  .controller('HomeCtrl', function() {
   });
--- src/test/controllers/home.controller.spec.js
+++ src/test/controllers/home.controller.spec.js
@@ -1,21 +1,16 @@
 'use strict';
 
 describe('home.controller', function(){
-  var state;
 
   beforeEach(module('MasterPlan'));
-  beforeEach(module('stateMock'));
 
   beforeEach(inject(function($injector) {
     state = $injector.get('$state');
   }));
 
-  it('should go to /dashboard if user is signed in', inject(function($controller) {
+  it('should do something', inject(function($controller) {
     $controller('HomeCtrl', {
-      User: { sessionData: '' },
-      $state: state
     });
-    state.expectTransitionTo('home.dashboard');
 
   }));
 });
