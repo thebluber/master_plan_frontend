450156f42d0c4c177c77c3deb9bdcedb9ad44bcc
Jiayi Zheng <thebluber@gmail.com>
2015-07-05 02:27:51 +0200

    Fix home controller test

--- src/test/controllers/home.controller.spec.js
+++ src/test/controllers/home.controller.spec.js
@@ -5,7 +5,6 @@ describe('home.controller', function(){
   beforeEach(module('MasterPlan'));
 
   beforeEach(inject(function($injector) {
-    state = $injector.get('$state');
   }));
 
   it('should do something', inject(function($controller) {
