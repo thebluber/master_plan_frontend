c4c782def0cc85df86d83327ebf461e5e3e68326
Jiayi Zheng <thebluber@gmail.com>
2015-07-14 18:40:12 +0200

    Add toggleForDate function in task service which should check the given task for the given date as done or undone

--- src/app/services/task.service.js
+++ src/app/services/task.service.js
@@ -5,8 +5,20 @@ angular.module('MasterPlan')
     var that = this;
 
     this.tasksForDate = function(date){
-      //bring Date obj to yyyy-mm-dd format
-      date = date.toJSON().split('T')[0];
-      return Restangular.one('tasks/for_date/' + date).get();
+      return Restangular.one('tasks/for_date/' + stringifyDate(date)).get();
     };
+
+    this.toggleForDate = function(task, date){
+      if(task.done){
+        //check task for the given date as undone
+        return Restangular.one('tasks/' + task.id + '/uncheck_for_date/' + stringifyDate(date)).remove();
+      } else {
+        //check task for the given date as done
+        return Restangular.one('tasks/' + task.id + '/check_for_date/' + stringifyDate(date)).post();
+      }
+    };
+
+    //bring Date obj to yyyy-mm-dd format
+    var stringifyDate = function(date) { return date.toJSON().split('T')[0]; };
+
   });
--- src/test/services/task.service.spec.js
+++ src/test/services/task.service.spec.js
@@ -18,4 +18,16 @@ describe('task.service', function(){
     taskService.tasksForDate(new Date(2015, 6, 5, 20));
     httpBackend.flush();
   });
+
+  //test toggleForDate
+  it('should check or uncheck the given task for the given date', function(){
+    httpBackend.expect('POST', '/api/v1/tasks/1/check_for_date/2015-07-05').respond([]);
+    taskService.toggleForDate({id: 1, done: false}, new Date(2015, 6, 5, 20));
+    httpBackend.flush();
+    httpBackend.expect('DELETE', '/api/v1/tasks/1/uncheck_for_date/2015-07-05').respond([]);
+    taskService.toggleForDate({id: 1, done: true}, new Date(2015, 6, 5, 20));
+    httpBackend.flush();
+  })
+  
+
 });
