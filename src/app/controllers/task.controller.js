321824d4c782ffa2ff53594e80f2d7afaf37b974
Jiayi Zheng <thebluber@gmail.com>
2015-07-16 14:19:41 +0200

    Fetch current_user from cookie in task controller Convert deadline to date object in task Store category_id and goal_id in task seperatly

--- src/app/controllers/task.controller.js
+++ src/app/controllers/task.controller.js
@@ -1,9 +1,15 @@
 'use strict';
 
 angular.module('MasterPlan')
-  .controller('TaskCtrl', function($scope, Restangular) {
+  .controller('TaskCtrl', function($scope, Restangular, $cookieStore) {
+    $scope.user = $cookieStore.get('current_user');
     Restangular.all('tasks').getList().then(function(response){
-      $scope.tasks = response;
+      $scope.tasks = _.map(response, function(task) {
+        task.deadline = new Date(task.deadline);
+        task.category_id = task.category.id;
+        if(task.goal) task.goal_id = task.goal.id;
+        return task
+      });
     }, function(error){
       console.log(error);
     });
--- src/test/controllers/task.controller.spec.js
+++ src/test/controllers/task.controller.spec.js
@@ -4,21 +4,38 @@ describe('task.controller', function() {
 
   var scope,
       httpBackend,
-      restangular;
+      cookies;
 
   beforeEach(module('MasterPlan'));
   beforeEach(module('app/views/home.html'));
   beforeEach(inject(function($injector) {
     scope = $injector.get('$rootScope');
     httpBackend = $injector.get('$httpBackend');
+    cookies = $injector.get('$cookieStore');
+    spyOn(cookies, 'get').and.returnValue({});
   }));
 
   it('should get all tasks of the user', inject(function($controller) {
     $controller('TaskCtrl', {
       $scope: scope
     });
+    console.log(cookies.get('tasks'));
+    httpBackend.expect('GET', '/api/v1/tasks').respond([{ category: {id: 1, name: 'work'}, deadline: '2015-07-15' }]);
+    httpBackend.flush();
+    expect(scope.tasks).toBeDefined();
+    //task deadline should be converted to date object
+    expect(scope.tasks[0].deadline instanceof Date).toBe(true);
+
+  }));
+
+  it('should have user in scope from cookies', inject(function($controller) {
+    $controller('TaskCtrl', {
+      $scope: scope
+    });
     httpBackend.expect('GET', '/api/v1/tasks').respond([]);
     httpBackend.flush();
+    expect(cookies.get).toHaveBeenCalled();
+    expect(scope.user).toEqual({});
   }));
 
 });
